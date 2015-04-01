5749fd00b18af888085a0e89774e888617641a6f
Julián Gutiérrez <juliangut@gmail.com>
2015-04-01 19:08:50 +0200

    doctrine handler middleware

--- src/DoctrineMiddleware.php
+++ src/DoctrineMiddleware.php
@@ -9,6 +9,10 @@
 namespace Jgut\Slim\Middleware;
 
 use Slim\Middleware;
+use Doctrine\Common\Annotations\AnnotationRegistry;
+use Doctrine\ORM\Tools\Setup;
+use Doctrine\ORM\Mapping\UnderscoreNamingStrategy;
+use Doctrine\ORM\EntityManager;
 
 /**
  * Doctrine handler middleware.
@@ -16,10 +20,120 @@ use Slim\Middleware;
 class DoctrineMiddleware extends Middleware
 {
     /**
+     * @var array
+     */
+    protected $options = [
+        'connection'             => null,
+        'annotation_paths'       => null,
+        'annotation_files'       => [],
+        'annotation_namespaces'  => [],
+        'annotation_autoloaders' => [],
+    ];
+
+     /**
+     * Check option availavility
+     *
+     * @param string $option
+     * @return bool
+     */
+    public function hasOption($option)
+    {
+        return isset($this->options[$option]);
+    }
+
+    /**
+     * Get option value or default if none existent
+     *
+     * @param string $option
+     * @param mixed $default
+     * @return mixed
+     */
+    public function getOption($option, $default = null)
+    {
+        return $this->hasOption($option) ? $this->options[$option] : $default;
+    }
+
+    /**
+     * Set option value
+     *
+     * @param string $option
+     * @param mixed $value
+     * @return $this
+     */
+    public function setOption($option, $value)
+    {
+        $this->options[$option] = $value;
+
+        return $this;
+    }
+
+    /**
+     * @param array $options
+     * @param array $defaults
+     *
+     * @return $this
+     */
+    public function setOptions(array $options, array $defaults = [])
+    {
+        $options = array_merge($defaults, $options);
+        $this->options = $options;
+
+        return $this;
+    }
+
+    /**
      * {@inheritDoc}
      */
     public function call()
     {
+        $this->setup();
+
         $this->next->call();
+    }
+
+    /**
+     * Set up Doctrine Entity Manager Slim service
+     */
+    public function setup()
+    {
+        $app = $this->getApplication();
+
+        $options = $app->config('doctrine');
+        if (is_array($options)) {
+            $this->setOptions($options, $this->options);
+        }
+
+        foreach ($this->getOption('annotation_files') as $file) {
+            AnnotationRegistry::registerFile($file);
+        }
+
+        foreach ($this->getOption('annotation_namespaces') as $namespaceMapping) {
+            AnnotationRegistry::registerAutoloadNamespace(reset($namespaceMapping), end($namespaceMapping));
+        }
+
+        foreach ($this->getOption('annotation_autoloaders') as $autoloader) {
+            AnnotationRegistry::registerLoader($autoloader);
+        }
+
+        $config = Setup::createConfiguration(!!$app->config('debug'));
+        $config->setNamingStrategy(new UnderscoreNamingStrategy(CASE_LOWER));
+
+        $annotationPaths = $this->getOption('annotation_paths');
+        if (empty($annotationPaths)) {
+            throw new \BadMethodCallException('annotation_paths config should be defined');
+        }
+
+        if (!is_array($annotationPaths)) {
+            $annotationPaths = array($annotationPaths);
+        }
+        $config->setMetadataDriverImpl($config->newDefaultAnnotationDriver($annotationPaths, false));
+
+        $connection = $this->getOption('connection');
+        $app->container->singleton(
+            'entity_manager',
+            function () use ($connection, $config) {
+                return EntityManager::create($connection, $config);
+            }
+        );
     }
 }
--- tests/MiddlewareTests/DoctrineMiddlewareTest.php
+++ tests/MiddlewareTests/DoctrineMiddlewareTest.php
@@ -9,11 +9,98 @@
 namespace Jgut\Slim\MiddlewareTests;
 
 use Jgut\Slim\Middleware\DoctrineMiddleware;
+use Slim\Helper\Set;
 
 /**
  * @covers Jgut\Slim\Middleware\DoctrineMiddleware
  */
 class DoctrineMiddlewareTest extends \PHPUnit_Framework_TestCase
 {
+    protected $middleware;
 
+    protected $doctrineConfig = [
+        'connection' => [
+            'driver' => 'pdo_sqlite',
+            'memory' => true,
+        ],
+        'annotation_files'       => [],
+        'annotation_namespaces'  => [],
+        'annotation_autoloaders' => [],
+    ];
+
+    public function setUp()
+    {
+        $config = $this->doctrineConfig;
+
+        $this->middleware = new DoctrineMiddleware();
+
+        $app = $this->getMock('Slim\\Slim', array(), array(), '', false);
+        $app->expects($this->any())->method('config')->will(
+            $this->returnCallback(
+                function () use ($config) {
+                    $args = array(
+                        'debug'    => true,
+                        'doctrine' => $config
+                    );
+                    return $args[func_get_arg(0)];
+                }
+            )
+        );
+
+        $this->middleware->setApplication($app);
+    }
+
+    /**
+     * @covers Jgut\Slim\Middleware\DoctrineMiddleware::getOption
+     * @covers Jgut\Slim\Middleware\DoctrineMiddleware::hasOption
+     */
+    public function testDefaults()
+    {
+        $this->assertNull($this->middleware->getOption('connection'));
+        $this->assertNull($this->middleware->getOption('annotation_paths'));
+        $this->assertEquals([], $this->middleware->getOption('annotation_files'));
+        $this->assertEquals([], $this->middleware->getOption('annotation_namespaces'));
+        $this->assertEquals([], $this->middleware->getOption('annotation_autoloaders'));
+    }
+
+    /**
+     * @covers Jgut\Slim\Middleware\DoctrineMiddleware::setOption
+     * @covers Jgut\Slim\Middleware\DoctrineMiddleware::getOption
+     */
+    public function testMutatorsAccessors()
+    {
+        $expected = [
+            sys_get_temp_dir() . '/tmp',
+        ];
+
+        $this->middleware->setOption('annotation_paths', $expected);
+
+        $this->assertEquals($expected, $this->middleware->getOption('annotation_paths'));
+    }
+
+    /**
+     * @covers Jgut\Slim\Middleware\DoctrineMiddleware::setup
+     * @expectedException \BadMethodCallException
+     */
+    public function testSetupNoPath()
+    {
+        $this->middleware->setup();
+    }
+
+    /**
+     * @covers Jgut\Slim\Middleware\DoctrineMiddleware::setup
+     */
+    public function testSetup()
+    {
+        $container = new Set();
+
+        $app = $this->middleware->getApplication();
+        $app->container = $container;
+        $this->middleware->setApplication($app);
+
+        $this->middleware->setOption('annotation_paths', 'sg');
+        $this->middleware->setup();
+
+        $this->assertInstanceOf('Doctrine\\ORM\\EntityManager', $container->get('entity_manager'));
+    }
 }
