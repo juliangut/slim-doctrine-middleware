668939a5804fa1ff74cb902eb1ffee5da62411bb
Matthias Crauwels <matthias.crauwels@ugent.be>
2015-08-10 16:58:04 +0200

    still learning to make good use of phpunit ;)

--- tests/MiddlewareTests/DoctrineCacheFactoryTest.php
+++ tests/MiddlewareTests/DoctrineCacheFactoryTest.php
@@ -20,15 +20,22 @@ class DoctrineCacheFactoryTest extends \PHPUnit_Framework_TestCase {
         $this->assertNull(DoctrineCacheFactory::configureCache(array('type' => '')));
         $this->assertInstanceOf('Doctrine\\Common\\Cache\\ApcCache', DoctrineCacheFactory::configureCache(array('type' => 'apc')));
         $this->assertInstanceOf('Doctrine\\Common\\Cache\\XcacheCache', DoctrineCacheFactory::configureCache(array('type' => 'xcache')));
-
-        if(extension_loaded('memcache')) {
-            $this->assertInstanceOf('Doctrine\\Common\\Cache\\MemcacheCache', DoctrineCacheFactory::configureCache(array('type' => 'memcache', 'host' => '127.0.0.1', 'port' => 11211)));
-        }
-
-        if(extension_loaded('redis')) {
-            $this->assertInstanceOf('Doctrine\\Common\\Cache\\RedisCache', DoctrineCacheFactory::configureCache(array('type' => 'redis', 'host' => '127.0.0.1', 'port' => 6379)));
-        }
-
         $this->assertInstanceOf('Doctrine\\Common\\Cache\\ArrayCache', DoctrineCacheFactory::configureCache(array('type' => 'array')));
+    }
+
+    public function testMemcacheCacheConfiguration() {
+        if(!extension_loaded('memcache')) {
+            $this->setExpectedException('\BadMethodCallException', 'MemcacheCache configured but module \'memcache\' not loaded.');
+        }
+        $this->assertInstanceOf('Doctrine\\Common\\Cache\\MemcacheCache', DoctrineCacheFactory::configureCache(array('type' => 'memcache', 'host' => '127.0.0.1', 'port' => 11211)));
+
+    }
+
+    public function testRedisCacheConfiguration() {
+        if(!extension_loaded('redis')) {
+            $this->setExpectedException('\BadMethodCallException', 'RedisCache configured but module \'redis\' not loaded.');
+        }
+        $this->assertInstanceOf('Doctrine\\Common\\Cache\\RedisCache', DoctrineCacheFactory::configureCache(array('type' => 'redis', 'host' => '127.0.0.1', 'port' => 6379)));
+
     }
 }
